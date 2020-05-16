167461ab79de9b4ee1f9ff0050bf88090d6420a6
x-lil-peep-x <ermateze@gmail.com>
2020-05-16 15:07:15 -0400

    Fixing Swal object in  Game Module.

--- src/app/game/components/game/game.component.ts
+++ src/app/game/components/game/game.component.ts
@@ -1,74 +1,79 @@
 import { AfterViewInit, Component, ElementRef, OnInit } from '@angular/core';
 
+declare let Swal: any;
+
 @Component({
-  selector: 'app-game',
-  templateUrl: './game.component.html',
-  styleUrls: ['./game.component.css']
+    selector: 'app-game',
+    templateUrl: './game.component.html',
+    styleUrls: ['./game.component.css']
 })
 export class GameComponent implements OnInit, AfterViewInit {
-  winnerSound = new Audio();
-  gameSound = new Audio();
-  playerTurnOriginal: boolean;
-  gameMode: string;
+    winnerSound = new Audio();
+    gameSound = new Audio();
+    playerTurnOriginal: boolean;
+    gameMode: string;
 
-  ngOnInit() {
+    ngOnInit() {
 
-    this.gameSound.load();
-    this.gameSound.play();
-  }
-  onChangeGameMode(gameMode) {
-    this.gameMode = gameMode;
-  }
-  constructor(
-    private elementRef: ElementRef
-  ) {
-    this.gameMode = null;
-    this.playerTurnOriginal = true;
-    this.gameSound.src = 'assets/sound/retro.mp3';
-    this.winnerSound.src = 'assets/sound/cheer.mp3';
-  }
-
-  ngAfterViewInit() {
-    // tslint:disable-next-line:max-line-length
-    this.elementRef.nativeElement.ownerDocument.body.style.backgroundImage = 'https://png.pngtree.com/thumb_back/fw800/back_pic/03/51/65/225791ee54c627b.jpg';
-  }
-
-  getPlayerPlaying() {
-    if (!this.playerTurnOriginal) {
-      return 'Turno jugador celeste';
-    } else {
-      return 'Turno jugador amarillo';
+        this.gameSound.load();
+        this.gameSound.play();
     }
-  }
 
-  onGameEnd(cell) {
-    this.winnerSound.load();
-    this.winnerSound.play();
-    const name = cell.content === 'plus' ? 'Amarillo' : 'Celeste';
-    const color = cell.content === 'plus' ? 'yellow' : '#1b5a8f';
-    const title = 'FELICIDADES !!!';
-    const text = `El ganador es el jugador ${name}`;
-    /* tslint: disable */
-    Swal.fire({
-      title,
-      text,
-      width: 490,
-      color: 'red',
-      height: 300,
-      padding: '3em',
-      confirmButtonText: 'Volver a Jugar',
-      background: color,
-      backdrop: `
+    onChangeGameMode(gameMode) {
+        this.gameMode = gameMode;
+    }
+
+    constructor(
+        private elementRef: ElementRef
+    ) {
+        this.gameMode = null;
+        this.playerTurnOriginal = true;
+        this.gameSound.src = 'assets/sound/retro.mp3';
+        this.winnerSound.src = 'assets/sound/cheer.mp3';
+    }
+
+    ngAfterViewInit() {
+        // tslint:disable-next-line:max-line-length
+        this.elementRef.nativeElement.ownerDocument.body.style.backgroundImage = 'https://png.pngtree.com/thumb_back/fw800/back_pic/03/51/65/225791ee54c627b.jpg';
+    }
+
+    getPlayerPlaying() {
+        if (!this.playerTurnOriginal) {
+            return 'Turno jugador celeste';
+        } else {
+            return 'Turno jugador amarillo';
+        }
+    }
+
+    onGameEnd(cell) {
+        this.winnerSound.load();
+        this.winnerSound.play();
+        const name = cell.content === 'plus' ? 'Amarillo' : 'Celeste';
+        const color = cell.content === 'plus' ? 'yellow' : '#1b5a8f';
+        const title = 'FELICIDADES !!!';
+        const text = `El ganador es el jugador ${name}`;
+        /* tslint: disable */
+
+        Swal.fire({
+            title,
+            text,
+            width: 490,
+            color: 'red',
+            height: 300,
+            padding: '3em',
+            confirmButtonText: 'Volver a Jugar',
+            background: color,
+            backdrop: `
         rgba(0,0,123,0.4)
         url("https://media1.tenor.com/images/9ac208d112c709a4980815f9f30ff6fc/tenor.gif?itemid=5665698")
         center top
         no-repeat        
       `,
-    }).then((res) => {
-      if (res) {
-        window.location.reload();
-      }
-    });
-  }
+        }).then((res) => {
+            if (res) {
+                window.location.reload();
+            }
+        });
+    }
 
 }
